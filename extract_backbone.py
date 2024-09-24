f8d5bec18bb48278c098817846664ee523daabe6
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-24 09:40:53 +0200

    run

---
+++ extract_backbone.py
@@ -0,0 +1,11 @@
+import torch
+
+model = torch.load('resnet50_fullckpt.pth', map_location="cpu")
+new_state_dict = {}
+"""for name,weights in model["state_dict"].items():
+    if name.startswith("module.encoder."):
+        name = name.replace("module.encoder.", "")
+        new_state_dict[name] = weights
+torch.save(new_state_dict, 'vicreg_resnet50.pth')"""
+for x in model:
+    print(x)
